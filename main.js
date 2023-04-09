024350bb9f1851dd5c31e75c6b1ed77e106cdc11
JFord <jamesford173@yahoo.com>
2023-04-09 20:17:02 +0100

    add kata and function start

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-/* 
-👉 Write your kata here!
-*/
+
+// 👉 Write your kata here!
+// write a function that tells you your heart rate training zone, so you know what your gains are when you're squatting every morning
+// your maximum heart rate is your age in years minused from 220
+// your 'heart rate reserve' is max heart rate minus your average resting heart rate 
+// if HR during squats is between 0 -50 % of 'heart rate reserve' this zone is not exercise
+// if HR during squats is between 50 -75 % of 'heart rate reserve'this is the fat burn zone
+// if HR during squats is between 75 -85 % of 'heart rate reserve' this is the aerobic endurance zone
+// if HR during squats is above 85 % of 'heart rate reserve' this is the anaerobic threshold zone
+
+// 
 
 //👉 Write the function your CodeWarriors will start with below here:
+  
+
+   export function squatGains(age, restingHeartRate, squattingHeartRate) {
+    const maxHeartRate = 220 - age;
+    const heartRateReserve = maxHeartRate - restingHeartRate;
+    const intensityPercentage = (squattingHeartRate - restingHeartRate) / heartRateReserve * 100;
+  
+    if (intensityPercentage <= 50) {
+      return "Not exercise";
+    } else if (intensityPercentage <= 75) {
+      return "Fat burn";
+    } else if (intensityPercentage <= 85) {
+      return "Aerobic endurance";
+    } else {
+      return "Anaerobic threshold";
+    }
+}
+
+const heartRateZone = squatGains(40, 70, 150);
+console.log(heartRateZone); 
+console.log("hi")
--- main.test.js
+++ main.test.js
@@ -1,1 +1,22 @@
 //👉 Write your tests below here:
+
+import { test, expect } from "@jest/globals";
+import { squatGains } from "./main.js";
+
+test("less than 50 should return 'Not exercise'", () => {
+    expect(squatGains(30, 60, 80)).toBe("Not exercise");
+  });
+  
+  test("less than 75 should return 'Fat burn'", () => {
+    expect(squatGains(30, 60, 140)).toBe("Fat burn");
+  });
+  
+  test("less than 85 should return 'Aerobic endurance'", () => {
+    expect(squatGains(30, 60, 160)).toBe("Aerobic endurance");
+  });
+  
+  test("above 85 should return 'Anaerobic threshold'", () => {
+    expect(squatGains(30, 60, 180)).toBe("Anaerobic threshold");
+  });
+  
+  
